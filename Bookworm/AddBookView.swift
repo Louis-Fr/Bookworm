ff0e668bf484a81bcbf245cac987237740851bed
Louis Rey <louisreypro@yahoo.com>
2023-01-27 12:35:28 +0100

    main

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,20 @@
+//
+//  AddBookView.swift
+//  BookwormBook
+//
+//  Created by Louis REY on 27/01/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
---
+++ Bookworm/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Louis REY on 27/01/2023.
+//
+
+import Foundation
