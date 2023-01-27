2455e21d533a93272547ba3429fe07c542e3f55e
Louis Rey <louisreypro@yahoo.com>
2023-01-27 21:43:18 +0100

    Add file and configure CoreData

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 @main
+
 struct BookwormApp: App {
+    
+    
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
