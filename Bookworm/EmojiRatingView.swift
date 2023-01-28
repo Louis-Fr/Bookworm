81cba8fe7678a473ebfadc68efbfa0798c882fa2
Louis Rey <louisreypro@yahoo.com>
2023-01-28 17:19:39 +0100

    main

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiRatingView.swift
+//  BookwormBook
+//
+//  Created by Louis REY on 28/01/2023.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView()
+    }
+}
