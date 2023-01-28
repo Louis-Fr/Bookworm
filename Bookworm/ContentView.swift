53fc823bb104c945c862e6c5b4c187ae372e59a6
Louis Rey <louisreypro@yahoo.com>
2023-01-28 17:32:50 +0100

    Modify ContentView

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -12,14 +12,34 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
     
-    
-    
     @State private var showingAddScreen = false
     
     var body: some View {
         
-        NavigationStack {
-            Text("Count : \(books.count)")
+        NavigationView {
+
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        Text(book.title ?? "Unknow")
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknow title")
+                                    .font(.headline)
+                           
+                            
+                            Text(book.author ?? "Unknow Author")
+                                .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+            
                 .navigationTitle("Bookworm")
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
